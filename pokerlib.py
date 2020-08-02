339798380071d991a87425a384f69f29076cf3b9
Sarah Fulkerson <contact@sarahfulkerson.com>
2020-08-01 21:04:47 -0500

    added classes
    created a Card class and added a placeholder for a Deck class

--- pokerlib.py
+++ pokerlib.py
@@ -2,9 +2,11 @@
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 suits = {'C':'Clubs', 'D':'Diamonds', 'H':'Hearts', 'S':'Spades'}
+royalflush = []
+straight = []
+fourofakind =  []
 
 # Royal Flush - possible combos
-royalflush = []
 for s in sorted(suits.values()):
     l = []
     for v in values[-5:]:
@@ -12,16 +14,38 @@ for s in sorted(suits.values()):
     royalflush.append(l)
 
 # Straight - possible combos
-straight = []
 for s in range(9):
     l = values[s:s+5]
     straight.append(l)
 
 # Four of a Kind - possible combos
-fourofakind =  []
 for v in values:
     l = []
     for s in sorted(suits.keys()):
         l.append('%s%s' %(v,s))
     fourofakind.append(l)
 # print(fourofakind)
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    def __eq__(self, other):
+        return values.index(self.value) == values.index(other.value)
+    def __ne__(self, other):
+        return values.index(self.value) != values.index(other.value)
+    def __gt__(self, other):
+        return values.index(self.value) > values.index(other.value)
+    def __ge__(self, other):
+        return values.index(self.value) >= values.index(other.value)
+    def __lt__(self, other):
+        return values.index(self.value) < values.index(other.value)
+    def __le__(self, other):
+        return values.index(self.value) <= values.index(other.value)
+    def getValue(self):
+        return self.value
+    def getSuit(self):
+        return self.suit
+
+class Deck:
+    pass
