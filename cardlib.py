8e3b40ff7d653cf7f72aabf6bbf90c68c02b14e4
Sarah Fulkerson <contact@sarahfulkerson.com>
2020-08-28 19:47:32 -0500

    switched from delegation to inheritance
    switched my Hand class around a bit. now instead of delegating to a list object, it inherits from class list and extends by adding custom methods to handle instances of class Card. also updated the logic to handle an arbitrary number of Card instances.

--- cardlib.py
+++ cardlib.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 # https://projecteuler.net/problem=54
 
-# TODO: abstract base classes!
-from collections.abc import MutableSequence
 from pokerlib import countOfValues, isHighCard, isOnePair, isTwoPairs, isThreeOfAKind, isStraight, isFlush, isFullHouse, isFourOfAKind, isStraightFlush, isRoyalFlush, handrankfuncs
 from utillib import values, suits, sortedOnIndex
 
@@ -103,49 +101,26 @@ class Card:
     def __str__(self):
         return "%s: value = '%s', suit = '%s'" % (self.__class__.__name__, self.value, suits.get(self.suit))
 
-class Hand:
+class Hand(list):
     """
-    This class takes 5 instances of class Card as arguments.
-
-    Attributes:
-    -- card1 (instance of class Card)
-    -- card2 (instance of class Card)
-    -- card3 (instance of class Card)
-    -- card4 (instance of class Card)
-    -- card5 (instance of class Card)
-    -- hand (list of 5 Card instances above)
-    -- sort (method for sorting Hand in place)
+    This class extends the 'list' built-in type and adds methods for processing
+    instances of class Card.
     """
-    def __init__(self, card1, card2, card3, card4, card5):
-        self.card1 = card1
-        self.card2 = card2
-        self.card3 = card3
-        self.card4 = card4
-        self.card5 = card5
-        self.hand = [card1, card2, card3, card4, card5]
-    def sort(self):
-        """
-        Sorts the Hand in place in ascending order and return None.
-        """
-        hand = self.hand[:]
-        n = len(hand)
-        for i in range(n-1):
-            for j in range(0, n-i-1):
-                if hand[j] > hand[j+1]:
-                    hand[j], hand[j+1] = hand[j+1], hand[j]
-        self.card1, self.card2, self.card3, self.card4, self.card5 = hand[0], hand[1], hand[2], hand[3], hand[4]
+    def __init__(self, *pargs):
+        list.__init__([])
+        self.extend([*pargs])
     def getValues(self, *, distinctvalues=False):
         """
         Returns the values of all Cards in the Hand.
         """
         v = []
         if distinctvalues == True:
-            for c in self.hand:
+            for c in self:
                 val = c.getValue()
                 if val not in v:
                     v.append(val)
         else:
-            for c in self.hand:
+            for c in self:
                 v.append(c.getValue())
         return v
     def getSuits(self, *, distinctsuits=False):
@@ -154,62 +129,38 @@ class Hand:
         """
         s = []
         if distinctsuits == True:
-            for c in self.hand:
+            for c in self:
                 val = c.getSuit()
                 if val not in s:
                     s.append(val)
         else:
-            for c in self.hand:
+            for c in self:
                 s.append(c.getSuit())
         return s
     def getHighCard(self):
         handvalues = sortedOnIndex(self.getValues(), values)
         return handvalues[-1]
-    def __getitem__(self, key):
-        """Fetches the Card at the given index."""
-        return self.hand[key]
-    def __setitem__(self, key, value):
-        #TODO
-        pass
-    def __delitem__(self, key):
-        #TODO
-        pass
-    def __contains__(self, key):
-        #
-        pass
-    def __add__(self, value):
-        #TODO
-        pass
-    def __iadd__(self, value):
-        #TODO
-        pass
-    def __len__(self):
-        """Returns the length of 'self.hand'."""
-        return len(self.hand)
-    def __iter__(self):
-        """Returns an instance of iterator class 'hand_iterator'."""
-        return hand_iterator(self.hand)
-    def __repr__(self):
-        return '%s(%s, %s, %s, %s, %s)' % (self.__class__.__name__,  repr(self.card1), repr(self.card2), repr(self.card3), repr(self.card4), repr(self.card5))
     def __str__(self):
-        return '%s:\ncard1 = [%s],\ncard2 = [%s],\ncard3 = [%s],\ncard4 = [%s],\ncard5 = [%s]' % (self.__class__.__name__,self.card1, self.card2, self.card3, self.card4, self.card5)
+        result = "%s:\n" % self.__class__.__name__
+        pos = 1
+        for c in self:
+            result = '%s%s\t%s\n' % (result, pos, str(c))
+            pos += 1
 
-class hand_iterator:
-    """
-    Iterator returned for the Hand iterable class.
-    """
-    def __init__(self, hand):
-        self.hand = hand
-        self.counter = 0
-    def __next__(self):
-        """
-        Returns the next Card in the Hand until the Hand is empty.
-        """
-        if self.counter == len(self.hand):
-            raise StopIteration
-        n = self.hand[self.counter]
-        self.counter += 1
-        return n
+        return result
+    def __repr__(self):
+        result = self.__class__.__name__
+        tup = tuple()
+
+        for c in self:
+             tup += (c,)
+        
+        if len(tup) == 1:
+            result = result + repr(tup)[:-2] + ')'
+        else:
+            result = result + repr(tup)
+        
+        return result
 
 class Deck:
     # [Card(x,y) for x in suits for y in values]
@@ -221,8 +172,8 @@ if __name__ == '__main__':
     c = Card('q', 'd')
     d = Card('j', 'd')
     e = Card('t', 'd')
-    h = Hand(a, b, c, d, e)
+    h = Hand(a,b,c,d,e)
     print('%s : %s' % ('~~~Hand type'.ljust(15, '~'), '~~~Result'.ljust(13, '~')))
     for func in handrankfuncs:
         print("%s : %s" % (func.__name__.ljust(15), func(h)))
-    print(h.getHighCard())
+    
